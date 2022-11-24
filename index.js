10d35fb6c2e92d021582c6fbd7899a70fe31d565
Hubert Jr <hubertjunior254@icloud.com>
2022-11-24 10:28:13 +0300

    work done

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+ let result;
+ if (someValue <= 400){
+  return "This one is on me!"
+ } else if (someValue > 2500) {
+  return "No can do."
+ } else if (someValue > 2000) {
+  return "I will gladly take your thirty bucks."
+ }
+ 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(someCity){
+  if (someCity === "NYC"){
+    return "Ok, sounds good."
+  } else {
+    return "No go."
+  }
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(MoneyTip){
+  if (MoneyTip === "generous"){
+    return "Thank you so much."
+  } else if (MoneyTip === "not as generous"){
+    return "Thank you."
+  } else {
+    return "Bye."
+  }
 }
